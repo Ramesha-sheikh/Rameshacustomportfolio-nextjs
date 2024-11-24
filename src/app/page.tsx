f08aa004d44bd3e478d39d9614cca54cfed0b0c5
Ramesha javed <165018589+Ramesha-sheikh@users.noreply.github.com>
2024-11-25 02:13:32 +0500

    new work

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Hero from '@/components/Herosection/hero'
+import Homesec from '@/app/Home/page'
+
+// import Contact from '@/app/Contact/page'
+export default function Home() {
+  return (
+    <>
+    {/* <Hero/> */}
+    <Homesec/>
+  
+   
+ 
+ 
+    </>
+  );
+}
